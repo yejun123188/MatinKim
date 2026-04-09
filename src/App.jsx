eb7161501b9d06ec595142e9572ef32239d9a7ab
yejun123188 <yejun123188@gmail.com>
2026-04-09 17:22:04 +0900

    헤더&푸터 (컴포넌트) / 홈 (페이지) 생성 후 APP 연동 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Route>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Footer />
     </>
   );
 }
