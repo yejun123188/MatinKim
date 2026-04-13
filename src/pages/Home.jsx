9ba0124d8b3e22fa16ba2720ebada6b000038a37
hayoung1109 <hadoll201109@gamil.com>
2026-04-13 18:35:19 +0900

    Edit,Flagship 섹션

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Flagship from "../components/Flagship";
+import TheEdit from "../components/TheEdit";
 
 export default function Home() {
   return (
     <div className="inner">
-      <h2>Home</h2>
+      <main>
+        {/* 퀵메뉴 */}
+        {/* 신상품 */}
+        {/* 베스트 아이템 */}
+        {/* 더 에딧 */}
+        <TheEdit />
+        {/* 플래그쉽 */}
+        <Flagship />
+        {/* 프로젝트 */}
+        {/* 인스타 */}
+
+      </main>
     </div>
   );
 }
