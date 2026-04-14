b2af2fe99dfb69316591df3e2f05a09ed89d3e3e
mhmte123 <mhmte123@gmail.com>
2026-04-14 15:49:40 +0900

    0414 퀵메뉴 제작완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import Instagram from '../components/Instagram';
 
 import Flagship from "../components/Flagship";
 import TheEdit from "../components/TheEdit";
+import ShortCut from '../components/ShortCut';
 
 export default function Home() {
     return (
         <div className="inner">
             <main>
                 {/* 퀵메뉴 */}
+                <ShortCut />
                 {/* 신상품 */}
                 {/* 베스트 아이템 */}
                 {/* 더 에딧 */}
