8474a5eb2b23ba1a1558fb91e0fea265d844b78c
hayoung1109 <hadoll201109@gamil.com>
2026-04-15 11:48:02 +0900

    로그인페이지클릭

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,97 +26,100 @@ const photoMenu = [
 export default function Header() {
   const { menus } = useProductStore();
   const [isShopHovered, setIsShopHovered] = useState(false);
+  //로그인창 열고 닫기
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
 
   return (
-    <header>
-      <div className="header-show">
-        <div className="inner">
-          <div className="header-left">
-            <h1><Link to={"/"}><img src="/images/header/logo-MatinKim-black.svg" alt="로고" /></Link></h1>
-            <nav>
-              <ul className="main-menu">
-                {topmenus.map((menu, id) => (
-                  <li
-                    key={id}
-                    onMouseEnter={() => menu.key === "shop" && setIsShopHovered(true)}
-                    onMouseLeave={() => menu.key === "shop" && setIsShopHovered(false)}
-                  >
-                    <Link to={`/${menu.key}`}>{menu.label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-          <div className="header-right">
-            <ul className="gnb-list">
-              <li><input type="text" placeholder='SEARCH' /></li>
-              <li className='cart'>
-                <Link to={"/cart"}>
-                  <img src="/images/header-icon/cart.svg" alt="" />
-                  <span className="cart-num">
-                    <span>1</span>
-                  </span>
-                </Link>
-              </li>
-              <li className='member'>
-                <Link to={"/member"}><img src="/images/header-icon/user.svg" alt="" />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div
-          className={`header-active ${isShopHovered ? "active" : ""}`}
-          onMouseEnter={() => setIsShopHovered(true)}
-          onMouseLeave={() => setIsShopHovered(false)}
-        >
+    <>
+      <header>
+        <div className="header-show">
           <div className="inner">
-            <div className="header-active-left">
-              <ul className="default-menu">
-                {defaultMenus.map((m, id) => (
-                  <li key={id}><Link to={m.link}>{m.name}</Link></li>
-                ))}
+            <div className="header-left">
+              <h1><Link to={"/"}><img src="/images/header/logo-MatinKim-black.svg" alt="로고" /></Link></h1>
+              <nav>
+                <ul className="main-menu">
+                  {topmenus.map((menu, id) => (
+                    <li
+                      key={id}
+                      onMouseEnter={() => menu.key === "shop" && setIsShopHovered(true)}
+                      onMouseLeave={() => menu.key === "shop" && setIsShopHovered(false)}
+                    >
+                      <Link to={`/${menu.key}`}>{menu.label}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <div className="header-right">
+              <ul className="gnb-list">
+                <li><input type="text" placeholder='SEARCH' /></li>
+                <li className='cart'>
+                  <Link>
+                    <img src="/images/header-icon/cart.svg" alt="" />
+                    <span className="cart-num">
+                      <span>1</span>
+                    </span>
+                  </Link>
+                </li>
+                <li className='member'>
+                  <button onClick={() => setIsLoginOpen(true)}><img src="/images/header-icon/user.svg" alt="" />
+                  </button>
+                </li>
               </ul>
             </div>
-            <div className="header-active-middle">
-              <ul className="main-menu">
-                {menus.map((menu, id) => (
-                  <li key={id}>
-                    {menu.name}
-                    <ul className="sub-menu">
-                      {menu.subMenu.map((m, id) => (
-                        <li key={id}>
-                          <Link to={m.link}>
-                            <p>{m.name}</p>
-                            <p className='e-sub-menu'>{m.subName}</p>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
+          </div>
+
+          <div
+            className={`header-active ${isShopHovered ? "active" : ""}`}
+            onMouseEnter={() => setIsShopHovered(true)}
+            onMouseLeave={() => setIsShopHovered(false)}
+          >
+            <div className="inner">
+              <div className="header-active-left">
+                <ul className="default-menu">
+                  {defaultMenus.map((m, id) => (
+                    <li key={id}><Link to={m.link}>{m.name}</Link></li>
+                  ))}
+                </ul>
+              </div>
+              <div className="header-active-middle">
+                <ul className="main-menu">
+                  {menus.map((menu, id) => (
+                    <li key={id}>
+                      {menu.name}
+                      <ul className="sub-menu">
+                        {menu.subMenu.map((m, id) => (
+                          <li key={id}>
+                            <Link to={m.link}>
+                              <p>{m.name}</p>
+                              <p className='e-sub-menu'>{m.subName}</p>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="header-active-right">
+                {photoMenu.map((m, id) => (
+                  <Link key={id}>
+                    <div className="img-box">
+                      <img src={m.src} alt="" />
+                    </div>
+                    <div className="text-box">
+                      <p>{m.subtitle}</p>
+                      <h3>{m.title}</h3>
+                    </div>
+                  </Link>
                 ))}
-              </ul>
-            </div>
-            <div className="header-active-right">
-              {photoMenu.map((m, id) => (
-                <Link key={id}>
-                  <div className="img-box">
-                    <img src={m.src} alt="" />
-                  </div>
-                  <div className="text-box">
-                    <p>{m.subtitle}</p>
-                    <h3>{m.title}</h3>
-                  </div>
-                </Link>
-              ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
-      <Login />
-
+      </header>
+      {isLoginOpen && <Login onClose={() => setIsLoginOpen(false)} />}
     </>
+
   )
 }
