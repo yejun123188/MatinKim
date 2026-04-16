f8c338feec48ad47315525d1745465cacee844a8
hayoung1109 <hadoll201109@gamil.com>
2026-04-16 17:05:14 +0900

    컬렉션/어바웃 디테일페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import About from "./pages/About";
 import { useProductStore } from "./store/useProductStore";
 import { useEffect } from "react";
 import UserInfo from "./pages/UserInfo";
+import CollectionDetail from "./pages/CollectionDetail";
+import ProjectDetail from "./pages/ProjectDetail";
 
 
 function App() {
@@ -29,7 +31,9 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/project" element={<Project />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/collections" element={<Collections />} />
+        <Route path="/collections/:id" element={<CollectionDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="/userInfo" element={<UserInfo />} />
       </Routes>
