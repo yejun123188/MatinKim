0d0ffdf0b90c413c1703b7faebf00f375a118bbd
hayoung1109 <hadoll201109@gamil.com>
2026-04-30 13:01:01 +0900

    위시,장바구니팝업

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Brand from './Brand'
 import "./scss/About.scss"
 
 export default function About() {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, []);
+    const [btnactive, setBtnActive] = useState(true);
     return (
         <section className='sub-section about'>
             <div className="button-box inner">
 
                 <ul className="button-list">
-                    <li><Link to={"brand"} >BRAND & INFO</Link ></li>
-                    <li><Link to={"stockist"}>STOCKIST</Link></li>
+                    <li className={btnactive ? "active" : ""} onClick={() => setBtnActive(true)}><Link to={"brand"} >BRAND & INFO</Link ></li>
+                    <li className={btnactive ? "" : "active"} onClick={() => setBtnActive(false)}><Link to={"stockist"}>STOCKIST</Link></li>
                 </ul>
             </div>
             <Outlet></Outlet>
