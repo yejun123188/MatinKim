4c778e1d1b1466641b33ce002bb313d015227022
KIMHOJUNG09 <muchlovee09@naver.com>
2026-05-07 18:10:09 +0900

    수정완료

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ import Agreement from "./pages/Agreement";
 import Cart from "./pages/Cart";
 import QnaWrite from "./pages/QnaWrite";
 import InquiryList from "./components/InquiryList";
+import FindId from "./pages/FindId";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -102,6 +103,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/board" element={<QnaWrite />} />
         <Route path="/inquiry/write" element={<InquiryList />} />
+        <Route path="/find" element={<FindId />} />
       </Routes>
       <Footer />
     </>
