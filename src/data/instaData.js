948b95f4519af018b6c209359e2cf34f1355a5ac
mhmte123 <mhmte123@gmail.com>
2026-05-13 18:06:41 +0900

    플래그쉽/인스타섹션 수정

--- src/data/instaData.js
+++ src/data/instaData.js
@@ -1,7 +1,18 @@
 export const instaData = [
-    { id: 1, imgUrl: "/images/insta-icon/Rectangle1.jpg", linkUrl: "https://www.instagram.com/p/DPFpDwfkvcg/", hash: ["26", "Spring", "Collection"], heartCheck: false },
-    { id: 2, imgUrl: "/images/insta-icon/Rectangle2.jpg", linkUrl: "https://www.instagram.com/p/DVsW1zDE4lP/", hash: ["26", "Spring", "Collection"], heartCheck: false },
-    { id: 3, imgUrl: "/images/insta-icon/Rectangle3.jpg", linkUrl: "https://www.instagram.com/p/DT99QpOEqJE/?img_index=1", hash: ["26", "Spring", "Collection"], heartCheck: false },
-    { id: 4, imgUrl: "/images/insta-icon/Rectangle4.jpg", linkUrl: "https://www.instagram.com/p/DWh5PZ-za8s/", hash: ["26", "Spring", "Collection"], heartCheck: false },
-    { id: 5, imgUrl: "/images/insta-icon/Rectangle5.jpg", linkUrl: "https://www.instagram.com/p/DUm1cXkkq7C/?img_index=1", hash: ["26", "Spring", "Collection"], heartCheck: false },
+    { id: 1, imgUrl: "/images/insta-icon/Rectangle1.jpg", linkUrl: "https://www.instagram.com/p/DPFpDwfkvcg/", hash: ["25", "Autumn", "Collection"], heartCheck: false },
+    { id: 2, imgUrl: "/images/insta-icon/Rectangle2.jpg", linkUrl: "https://www.instagram.com/p/DVsW1zDE4lP/", hash: ["24", "Summer", "Collection"], heartCheck: false },
+    { id: 3, imgUrl: "/images/insta-icon/Rectangle3.jpg", linkUrl: "https://www.instagram.com/p/DT99QpOEqJE/?img_index=1", hash: ["26", "Winter", "Collection"], heartCheck: false },
+    { id: 4, imgUrl: "/images/insta-icon/Rectangle4.jpg", linkUrl: "https://www.instagram.com/p/DJG1lNHpp-M/?img_index=1", hash: ["25", "Spring", "Collection"], heartCheck: false },
+    { id: 5, imgUrl: "/images/insta-icon/Rectangle5.jpg", linkUrl: "https://www.instagram.com/p/DUm1cXkkq7C/?img_index=1", hash: ["24", "Autumn", "Collection"], heartCheck: false },
+    { id: 6, imgUrl: "/images/insta-icon/Rectangle6.jpg", linkUrl: "https://www.instagram.com/p/DViYWMCktk7/?img_index=1", hash: ["26", "Summer", "Collection"], heartCheck: false },
+    { id: 7, imgUrl: "/images/insta-icon/Rectangle7.jpg", linkUrl: "https://www.instagram.com/p/CwPbwY8OLeS/?img_index=1", hash: ["25", "Winter", "Collection"], heartCheck: false },
+    { id: 8, imgUrl: "/images/insta-icon/Rectangle8.jpg", linkUrl: "https://www.instagram.com/p/DVdOuxkkgTp/?img_index=1", hash: ["26", "Spring", "Collection"], heartCheck: false },
+    { id: 9, imgUrl: "/images/insta-icon/Rectangle9.jpg", linkUrl: "https://www.instagram.com/p/DPLiwm-EvpR/", hash: ["24", "Summer", "Collection"], heartCheck: false },
+    { id: 10, imgUrl: "/images/insta-icon/Rectangle10.jpg", linkUrl: "https://www.instagram.com/p/DOqZgiyEteW/?img_index=1", hash: ["25", "Autumn", "Collection"], heartCheck: false },
+    { id: 11, imgUrl: "/images/insta-icon/Rectangle11.jpg", linkUrl: "https://www.instagram.com/p/DUDLOioEp_e/?img_index=1", hash: ["24", "Winter", "Collection"], heartCheck: false },
+    { id: 12, imgUrl: "/images/insta-icon/Rectangle12.jpg", linkUrl: "https://www.instagram.com/p/DEzJiw4p3lx/?img_index=1", hash: ["26", "Spring", "Collection"], heartCheck: false },
+    { id: 13, imgUrl: "/images/insta-icon/Rectangle13.jpg", linkUrl: "https://www.instagram.com/p/C0OdEp2J27I/?img_index=1", hash: ["25", "Summer", "Collection"], heartCheck: false },
+    { id: 14, imgUrl: "/images/insta-icon/Rectangle14.jpg", linkUrl: "https://www.instagram.com/p/DEoz-VjJLhm/?img_index=1", hash: ["24", "Autumn", "Collection"], heartCheck: false },
+    { id: 15, imgUrl: "/images/insta-icon/Rectangle15.jpg", linkUrl: "https://www.instagram.com/p/DNpsfjfSxTf/?img_index=1", hash: ["26", "Winter", "Collection"], heartCheck: false },
+    { id: 16, imgUrl: "/images/insta-icon/Rectangle16.jpg", linkUrl: "https://www.instagram.com/p/DAS_qAPP-qv/?img_index=1", hash: ["25", "Spring", "Collection"], heartCheck: false }
 ]
