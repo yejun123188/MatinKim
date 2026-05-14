d83641c8a43903db038a28faa58542947488ec35
KIMHOJUNG09 <muchlovee09@naver.com>
2026-05-14 10:08:49 +0900

    scss수정

--- src/components/LegalMenu.jsx
+++ src/components/LegalMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./scss/LegalMenu.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function LegalMenu() {
     return (
@@ -8,11 +8,38 @@ export default function LegalMenu() {
             <h2>LEGAL</h2>
 
             <ul>
-                <li className="active"><Link to="/kimmatin/guide">GUIDE</Link></li>
-                <li> <Link to="/kimmatin/privacy-policy">
-                    PRIVACY POLICY
-                </Link></li>
-                <li><Link to="/kimmatin/terms">T&amp;C</Link></li>
+                <li>
+                    <NavLink
+                        to="/kimmatin/guide"
+                        className={({ isActive }) =>
+                            isActive ? "active" : ""
+                        }
+                    >
+                        GUIDE
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/kimmatin/privacy-policy"
+                        className={({ isActive }) =>
+                            isActive ? "active" : ""
+                        }
+                    >
+                        PRIVACY POLICY
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/kimmatin/terms"
+                        className={({ isActive }) =>
+                            isActive ? "active" : ""
+                        }
+                    >
+                        T&amp;C
+                    </NavLink>
+                </li>
             </ul>
         </div>
     );
