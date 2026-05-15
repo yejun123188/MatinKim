f2062c618abaaf2b7bc2f9c3a89dcc19f9f43f89
yejun123188 <yejun123188@gmail.com>
2026-05-15 15:38:01 +0900

    푸터

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,7 +62,10 @@ export default function Footer() {
                       }
                     >
                       <span>{menu.title}</span>
-                      <span className="footer-dropdown-icon" aria-hidden="true" />
+                      <span
+                        className="footer-dropdown-icon"
+                        aria-hidden="true"
+                      />
                     </button>
 
                     {openMenu === menu.title && (
@@ -79,8 +82,15 @@ export default function Footer() {
               </ul>
 
               <address className="footer-address">
-                <p>(주)마뗑킴 | 대표이사 : 홍길동 / 경기 성남시 분당구 판교로 242 더샵 702호 / 통신판매업신고번호 : 2025-성남분당A-0780</p>
-                <p>사업자등록번호 : 743-88-00954 <Link to="/">[사업자정보확인]</Link> / 개인정보보호책임자 : 이지은</p>
+                <p>
+                  (주)마뗑킴 | 대표이사 : 홍길동 / 경기 성남시 분당구 판교로 242
+                  더샵 702호 / 통신판매업신고번호 : 2025-성남분당A-0780
+                </p>
+                <p>
+                  사업자등록번호 : 743-88-00954{" "}
+                  <Link to="/">[사업자정보확인]</Link> / 개인정보보호책임자 :
+                  이지은
+                </p>
                 <p>비즈니스 관련문의 : matinkimcrew@matinkim.com</p>
               </address>
 
@@ -90,13 +100,25 @@ export default function Footer() {
             <div className="footer-right">
               <ul className="sns-list">
                 <li>
-                  <Link to="https://www.instagram.com/kimmatin_magazine/" aria-label="Instagram">
-                    <img src="/images/footer-icon/imstagram-icon.svg" alt="Instagram" />
+                  <Link
+                    to="https://www.instagram.com/kimmatin_magazine/"
+                    aria-label="Instagram"
+                  >
+                    <img
+                      src="/images/footer-icon/imstagram-icon.svg"
+                      alt="Instagram"
+                    />
                   </Link>
                 </li>
                 <li>
-                  <Link to="https://www.youtube.com/@MATINKIM-is2dm" aria-label="YouTube">
-                    <img src="/images/footer-icon/youtube-logo.svg" alt="YouTube" />
+                  <Link
+                    to="https://www.youtube.com/@MATINKIM-is2dm"
+                    aria-label="YouTube"
+                  >
+                    <img
+                      src="/images/footer-icon/youtube-logo.svg"
+                      alt="YouTube"
+                    />
                   </Link>
                 </li>
               </ul>
@@ -127,7 +149,8 @@ export default function Footer() {
             <img src="/images/footer-icon/logo-name.svg" alt="logo" />
           </h2>
           <p className="footer-slogan">
-            Matin Kim Strives To Harmonize A Distinctive Sense Of Freedom And A Rough Style
+            Matin Kim Strives To Harmonize A Distinctive Sense Of Freedom And A
+            Rough Style
             <br />
             Within The Diverse Tapestry Of Daily Fashion Cultures.
           </p>
@@ -136,17 +159,36 @@ export default function Footer() {
         <div className="footer-bottom">
           <div className="footer-left">
             <ul className="footer-menu">
-              <li><Link to="/">(주)마뗑킴</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/agreement">이용약관</Link></li>
-              <li><Link to="/privacy">개인정보처리방침</Link></li>
-              <li><Link to="/guide">이용안내</Link></li>
-              <li><Link to="/product-authentication">정품인증</Link></li>
+              <li>
+                <Link to="/">(주)마뗑킴</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/agreement">이용약관</Link>
+              </li>
+              <li>
+                <Link to="/privacy">개인정보처리방침</Link>
+              </li>
+              <li>
+                <Link to="/guide">이용안내</Link>
+              </li>
+              <li>
+                <Link to="/product-authentication">정품인증</Link>
+              </li>
             </ul>
 
             <address className="footer-address">
-              <p>(주)마뗑킴 | 대표이사 : 홍길동 / 경기 성남시 분당구 판교로 242 더샵 702호 / 통신판매업신고번호 : 2025-성남분당A-0780</p>
-              <p>사업자등록번호 : 743-88-00954 <Link to="/">[사업자정보확인]</Link> / 개인정보보호책임자 : 이지은</p>
+              <p>
+                (주)마뗑킴 | 대표이사 : 홍길동 / 경기 성남시 분당구 판교로 242
+                더샵 702호 / 통신판매업신고번호 : 2025-성남분당A-0780
+              </p>
+              <p>
+                사업자등록번호 : 743-88-00954{" "}
+                <Link to="/">[사업자정보확인]</Link> / 개인정보보호책임자 :
+                이지은
+              </p>
               <p>비즈니스 관련문의 : matinkimcrew@matinkim.com</p>
             </address>
 
@@ -156,13 +198,25 @@ export default function Footer() {
           <div className="footer-right">
             <ul className="sns-list">
               <li>
-                <Link to="https://www.instagram.com/matinkim_magazine/?hl=ko" aria-label="인스타그램">
-                  <img src="/images/footer-icon/imstagram-icon.svg" alt="인스타그램" />
+                <Link
+                  to="https://www.instagram.com/matinkim_magazine/?hl=ko"
+                  aria-label="인스타그램"
+                >
+                  <img
+                    src="/images/footer-icon/imstagram-icon.svg"
+                    alt="인스타그램"
+                  />
                 </Link>
               </li>
               <li>
-                <Link to="https://www.youtube.com/@MATINKIM-is2dm" aria-label="유튜브">
-                  <img src="/images/footer-icon/youtube-logo.svg" alt="유튜브" />
+                <Link
+                  to="https://www.youtube.com/@MATINKIM-is2dm"
+                  aria-label="유튜브"
+                >
+                  <img
+                    src="/images/footer-icon/youtube-logo.svg"
+                    alt="유튜브"
+                  />
                 </Link>
               </li>
             </ul>
